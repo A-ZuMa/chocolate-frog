ea8d421b14c18315334a3af1bfd7c1a9898ad647
A-ZuMa <azuley@gmail.com>
2023-06-13 12:29:43 +0300

    JS Fixes

--- main.js
+++ main.js
@@ -1,5 +1,181 @@
-import './js/modal-order';
-import './js/modal-review';
-import './js/modal-subscribe';
-import './js/mobile-menu';
-import './js/swiper.js';
+// MODAL ORDER SECTION
+(() => {
+  const refs = {
+    openModalBtn2: document.querySelector('[data-hero-open]'),
+    openModalBtn: document.querySelector('[data-modal-order-open]'),
+    closeModalBtn: document.querySelector('[data-modal-order-close]'),
+    modal: document.querySelector('[data-modal-order]'),
+  };
+
+  refs.openModalBtn2.addEventListener('click', toggleModal);
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
+
+// MODAL REVIEW SECTION
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-review-open]'),
+    closeModalBtn: document.querySelector('[data-modal-review-close]'),
+    modal: document.querySelector('[data-modal-review]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
+
+// MODAL SUBSCRIBE SECTION
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector("[data-modal-explore-open]"),
+      closeModalBtn: document.querySelector("[data-modal-explore-close]"),
+      modal: document.querySelector("[data-modal-explore]"),
+    };
+  
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle("is-hidden");
+    }
+})();
+  
+// MOBILE MENU SECTION
+(() => {
+  const mobileMenu = document.querySelector('.js-menu-container');
+  const openMenuBtn = document.querySelector('.js-open-menu');
+  const closeMenuBtn = document.querySelector('.js-close-menu');
+
+  const toggleMenu = () => {
+    const isMenuOpen =
+      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+    mobileMenu.classList.toggle('is-open');
+
+    const scrollLockMethod = !isMenuOpen
+      ? 'disableBodyScroll'
+      : 'enableBodyScroll';
+    bodyScrollLock[scrollLockMethod](document.body);
+  };
+
+  openMenuBtn.addEventListener('click', toggleMenu);
+  closeMenuBtn.addEventListener('click', toggleMenu);
+
+  // Close the mobile menu on wider screens if the device orientation changes
+  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+    if (!e.matches) return;
+    mobileMenu.classList.remove('is-open');
+    openMenuBtn.setAttribute('aria-expanded', false);
+    bodyScrollLock.enableBodyScroll(document.body);
+  });
+})();
+
+// SWIPER SECTION
+// js SwiperOurProducts
+const swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  loop: true,
+
+  slidesPerView: 1,
+  spaceBetween: 18,
+
+  breakpoints: {
+    //  >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 18,
+    },
+    //  >= 768px
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 18,
+    },
+
+    //  >= 1200px
+    1200: {
+      slidesPerView: 4,
+      spaceBetween: 18,
+    },
+  },
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  /*
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+  */
+});
+
+// const swiper = new Swiper('.swiper', {
+//   // Optional parameters
+//   direction: 'horizontal',
+//   loop: true,
+
+//   // If we need pagination
+//   pagination: {
+//     el: '.swiper-pagination',
+//   },
+
+//   // Navigation arrows
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+
+//   // And if we need scrollbar
+//   scrollbar: {
+//     el: '.swiper-scrollbar',
+//   },
+// });
+
+var reviewSwiper = new Swiper('.mySwiper', {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  slidesPerGroup: 1,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  breakpoints: {
+    //  >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 16,
+    },
+    //  >= 768px
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 18,
+    },
+
+    //  >= 1200px
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 28,
+    },
+  },
+  pagination: {
+    el: '.swiper-reviews-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
